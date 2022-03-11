212e1825b4a01d77dfd554e987852d17950411d3
kanolino <liedsoncanal@gmail.com>
2022-03-11 19:10:22 -0100

    #4 dynamic values in templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const title = 'Welcome to the new blog'
+    const likes = 50
+    // const person = { name: 'yoshi', age: '30'}
+    const link = 'http://google.com'
 
-  return (
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="conten">
+        <h1>{title}</h1>
+
+        <p>Liked {likes} times</p>
+        { /*<p> {person} </p>*/ }
+        
+        <p>{10}</p>
+        <p>{'hello ninjas'}</p>
+        <p>{ Math.random() * 10 }</p>
+      
+        <a href= {link}> Google site</a>
+
+      </div>
     </div>
   )
 }
