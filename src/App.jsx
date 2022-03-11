babc8f5e0089fc2e4795473510fbf05b3c8e885a
kanolino <liedsoncanal@gmail.com>
2022-03-11 19:45:52 -0100

    #5 multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-    const title = 'Welcome to the new blog'
-    const likes = 50
-    // const person = { name: 'yoshi', age: '30'}
-    const link = 'http://google.com'
-
    return (
     <div className="App">
-      <div className="conten">
-        <h1>{title}</h1>
-
-        <p>Liked {likes} times</p>
-        { /*<p> {person} </p>*/ }
-        
-        <p>{10}</p>
-        <p>{'hello ninjas'}</p>
-        <p>{ Math.random() * 10 }</p>
-      
-        <a href= {link}> Google site</a>
+      <Navbar/>
+      <div className="content">
+        <Home />
 
       </div>
     </div>
