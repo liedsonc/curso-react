8dcacb715bbbf03948e5f560a3e2fb5a2391e5d6
kanolino <liedsoncanal@gmail.com>
2022-03-12 12:50:10 -0100

    #7 click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello, ninjas')
+    }
+
+    const handleClickAgain = (name, e) => {
+            console.log('Hello ' + name, e.target)        
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={ (e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
      );
 }
