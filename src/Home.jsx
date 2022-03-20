7a7987d37cda2c0693709fe66152c74618946d85
kanolino <liedsoncanal@gmail.com>
2022-03-20 20:39:30 -0100

    #19 handling fetch

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,25 +4,42 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-      
     const [name,setName] = useState('mario')
-      
-   
-      
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null)
       useEffect(() =>{
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json()
-        })
-        .then (data => {
-          console.log(data)
-          setBlogs(data)
-        })
-      
+        setTimeout(() =>{
+          fetch('http://localhost:8000/blogs')
+           
+          .then(res => {
+              if(!res.ok){
+                throw Error('could not fetch the data for that resource')
+              }
+              console.log(res)
+              // if(res.ok){
+
+              // }
+              return res.json()
+            })
+           
+            .then (data => {
+              setBlogs(data)
+              setIsLoading(false)
+              setError(null)
+            })
+            
+            .catch(err =>{
+              setIsLoading(false)
+              setError(err.message)
+            })
+        }, 1000)
+
       }, [])
      
     return ( 
         <div className="home">
+          { error && <div>{ error }</div>}
+          {isLoading && <div>Loading...</div>}
           {blogs && <BlogList blogs={blogs} title='All Blogs!' /> }
           
         </div>
