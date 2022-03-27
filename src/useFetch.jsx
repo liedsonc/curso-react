fabcb19f3686ab484578a5b2449f965a77e3ebb1
liedsonc <liedsoncanal@gmail.com>
2022-03-27 00:12:36 -0100

    #24 useEfect cleanup

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -6,8 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() =>{
+      const abortCont =new AbortController();
         setTimeout(() =>{
-          fetch(url)
+          fetch(url, {signal: abortCont.signal})
            
           .then(res => {
               if(!res.ok){
@@ -24,11 +25,15 @@ const useFetch = (url) => {
             })
             
             .catch(err =>{
+               if(err.name === 'AbortError'){
+                console.log('fetch aborted');
+               } else {
               setIsLoading(false)
               setError(err.message)
+              }
             })
-        }, )
-
+        },1000 )
+        return () => abortCont.abort
       }, [url])
     
       return { data, isLoading, error}
