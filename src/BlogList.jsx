8772bf796a1645a05bb183c10f5c66dd0e8e4152
liedsonc <liedsoncanal@gmail.com>
2022-03-27 00:30:09 -0100

    #25 route parameters

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function BlogList({blogs}) {
 
     // const blogs = props.blogs;
@@ -7,8 +9,10 @@ function BlogList({blogs}) {
            
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
                      <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    </Link>
                     </div>
             ))}
         </div>
