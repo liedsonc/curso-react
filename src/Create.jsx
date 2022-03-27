ead46cc2e8b4585c0310b341f67f74f3a1b8263c
liedsonc <liedsoncanal@gmail.com>
2022-03-27 18:58:26 -0100

    #30 programmatic redirects

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-
+import { useHistory } from "react-router-dom"
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isLoading, setIsLoading] = useState(false)
+    const history = useHistory();
     
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,7 +20,8 @@ const Create = () => {
        }) .then(()=> {
            console.log('new blog added');
            setIsLoading(false);
-       })
+           history.push('/');
+       });
     }
     
     
