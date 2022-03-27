04ad53e812b7ac2bda0a8a1ed80fbe3f3cb4a69e
liedsonc <liedsoncanal@gmail.com>
2022-03-27 18:50:13 -0100

    #29 making a POST request

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,12 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isLoading, setIsLoading] = useState(false)
     
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog ={title, body, author};
 
-        console.log(blog);
+        setIsLoading(true);
+
+       fetch('http://localhost:8000/blogs', {
+           method: 'POST',
+           headers: {'Content-Type': 'application/json'},
+           body: JSON.stringify(blog)
+       }) .then(()=> {
+           console.log('new blog added');
+           setIsLoading(false);
+       })
     }
     
     
@@ -38,10 +48,11 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add blog</button>
-                <p>{ title }</p>
+                {!isLoading && <button>Add blog</button>}
+                {isLoading && <button>Adding blog...</button>}
+                {/* <p>{ title }</p>
                 <p>{ body }</p>
-                <p>{ author }</p>
+                <p>{ author }</p> */}
             </form>
 
         </div>
