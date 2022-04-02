579ee5a9d2168fc192b2ec87d65d02cf2b90512e
Liedson Correia <liedsoncanal@gmail.com>
2022-04-02 02:17:06 -0100

    refactor

--- src/Components/useFetch/useFetch.tsx
+++ src/Components/useFetch/useFetch.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState, useEffect} from 'react';
 
 
-
 const useFetch  = <T,> (url: RequestInfo )  => {
     const [data, setData] = useState<T | null>(null);
     const [isLoading, setIsLoading] = useState(true);
@@ -35,44 +34,9 @@ const useFetch  = <T,> (url: RequestInfo )  => {
               setError(err.message)
               }
             })
-        },1000 )
-
-        // return () => abortCont.abort
-            
+        },1000 )            
     },[url])
 
-
-  // useEffect () => {
-  //     const abortCont =new AbortController();
-  //       setTimeout(() =>{
-  //         fetch(url, {signal: abortCont.signal})
-           
-  //         .then(res => {
-  //             if(!res.ok){
-  //               throw Error('could not fetch the data for that resource')
-  //             }
-              
-  //             return res.json()
-  //           })
-           
-  //           .then (data => {
-  //             setData(data)
-  //             setIsLoading(false)
-  //             setError(null)
-  //           })
-            
-  //           .catch(err =>{
-  //              if(err.name === 'AbortError'){
-  //               console.log('fetch aborted');
-  //              } else {
-  //             setIsLoading(false)
-  //             setError(err.message)
-  //             }
-  //           })
-  //       },1000 )
-  //       return () => abortCont.abort
-  //     }
-    
       return { data, isLoading, error}
 }
 export default useFetch;
